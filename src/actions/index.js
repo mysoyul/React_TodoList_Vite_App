f663c9be03ee7f984a03a4800213741f922de756
mysoyul <vega2k_@naver.com>
2024-10-29 16:26:45 +0900

    REMOVE_TODO adtion reducer add

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,25 @@ import axios from 'axios';
 //Action type 정의
 export const FETCH_TODOS = "FETCH_TODOS";
 export const ADD_TODO = "ADD_TODO";
+export const REMOVE_TODO = "REMOVE_TODO";
 
 const apiUrl = 'http://localhost:4500/api/todos';
+
+//Todo 삭제 Action 함수
+export const removeTodo = id => {
+    return (dispatch) => {
+        axios.delete(`${apiUrl}/${id}`)
+            .then(res =>
+                dispatch({
+                    type: REMOVE_TODO,
+                    payload: res.data
+                }))
+            .catch(error => {
+                console.log(error);
+                throw (error);
+            })
+    }
+}; //removeTodo
 
 //Todo 등록 Action 함수
 export const addTodo = (todo) => {
