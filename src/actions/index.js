064943c847550a1447c0c3108138a4003231673e
mysoyul <vega2k_@naver.com>
2024-10-29 15:22:52 +0900

    actions fetchAllTodos add

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,18 @@ import axios from 'axios';
 export const FETCH_TODOS = "FETCH_TODOS";
 
 const apiUrl= 'http://localhost:4500/api/todos';
+
+//Todo 목록조회 Action 함수
+export const fetchAllTodos = () => {
+    return (dispatch) => {
+        axios.get(apiUrl) //Promise
+            .then(res => dispatch({
+                type: FETCH_TODOS,
+                payload: res.data
+            }))
+            .catch(error => {
+                console.log(error);
+                throw error;
+            });
+    }
+}; //fetchAllTodos
